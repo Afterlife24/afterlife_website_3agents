f34b6f56bf91f4a0640d7b1ed962b35b92a1fac4
harini-rapaka <rapakasundari843@gmail.com>
2026-02-20 23:49:23 +0530

    deploy to aws

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "standalone",           // generates server folder for Lambda
+  typescript: { ignoreBuildErrors: true },
 };
 
 export default nextConfig;
